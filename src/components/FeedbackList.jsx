c695344e92e3199abc6ef023397c5374ec12932d
Tung Le <tunglungtung87@gmail.com>
2022-04-06 23:08:20 +0700

    Optional with amimation with framer-motion. Preparing to using Routes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,40 @@
-import PropTypes from 'prop-types'
-import FeedbackItem from './FeedbackItem'
+import { motion, AnimatePresence } from 'framer-motion';
+import PropTypes from 'prop-types';
+import FeedbackItem from './FeedbackItem';
 
-function FeedbackList({feedback,handleDelete}) {
-
-  if(!feedback || feedback.length === 0)  {
-    return <p>No feedback yet</p>
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No feedback yet</p>;
   }
   return (
     <div className="feedback-list">
-      {feedback.map((feed) => (
-        <FeedbackItem key={feed.id} item={feed} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
-  )
+  );
+
+  /// Version without animation
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((feed) => (
+  //       <FeedbackItem key={feed.id} item={feed} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
@@ -23,6 +45,6 @@ FeedbackList.propTypes = {
       rating: PropTypes.number.isRequired
     })
   )
-}
+};
 
-export default FeedbackList
+export default FeedbackList;
